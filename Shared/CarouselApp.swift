493d8cdd96d376635f80612ffbdf22ab66db14a9
Mohammad Nazib -ul- Islam <nzbszb@gmail.com>
2020-12-29 10:00:34 +0600

    Initial Commit

---
+++ Shared/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Shared
+//
+//  Created by Admin on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
